4cbf63a58f1cdea9a0a474bdb048f8921d4ca5af
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-06-04 15:38:47 +0530

    Update Graph.r

--- Graph.r
+++ Graph.r
@@ -3,3 +3,9 @@
 a <- c(20 , 30 , 40 ,50)
 m <- c("mar","apr","may","june")
 barplot(a , names.arg = m, xlab="month",ylab="Revenue",col="white",main="revenue chart",border="black")
+
+#bargraph Revenue 
+colors<-c("green","white","orange")
+months<-c("jan","feb","mar","apr")
+values<-matrix(c(1,2,3,4,5,6,7,8,9,10,11,12), nrow = 3 , ncol=4 , byrow =TRUE)
+barplot(values,main = "revenue chart" , names.arg=months, xlab="Month" , ylab = "Revenue" , col = colors)
