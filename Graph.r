a3c051fb0d98367d521ae0309b135d1c3c4f09b8
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-06-04 15:56:05 +0530

    Update Graph.r

--- Graph.r
+++ Graph.r
@@ -9,3 +9,8 @@ colors<-c("green","white","orange")
 months<-c("jan","feb","mar","apr")
 values<-matrix(c(1,2,3,4,5,6,7,8,9,10,11,12), nrow = 3 , ncol=4 , byrow =TRUE)
 barplot(values,main = "revenue chart" , names.arg=months, xlab="Month" , ylab = "Revenue" , col = colors)
+
+#histogram
+v<-c(1,5,7,8, 11, 15, 19, 25, 27 , 30 , 35 ,40)
+months<-c("jan","feb","mar","apr","may")
+hist(v,xlab="month", ylab = "tree", col="green",border = "red", breaks = 3 , xlim=c(0,40), ylim=c(0,5))
